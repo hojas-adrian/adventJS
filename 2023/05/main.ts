89470cd88fbf302d3abdbe1e27db76971251041c
hojas.adrian <hojas.adrian@gmail.com>
2023-12-05 18:36:51 -0500

    Refactor: Change map for forEach

--- 2023/05/main.ts
+++ 2023/05/main.ts
@@ -1,14 +1,14 @@
 export function cyberReindeer(road: string, time: number) {
   // Code here
   const output: string[] = [];
-  let steps = road.split("");
+  const steps = road.split("");
   let lastStep = ".";
 
   for (let i = 0; i < time; i++) {
     output.push(steps.join(""));
     if (i >= 4) {
-      steps = steps.map((step) => {
-        return step === "|" ? "*" : step;
+      steps.forEach((step, index) => {
+        steps[index] = step === "|" ? "*" : step;
       });
     }
 
